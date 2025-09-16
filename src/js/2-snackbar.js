06d3681d892cdcf6cf2879e3bc6d03d97ca0e62d
Olena Petrova <petrovahel@icloud.com>
2025-09-16 22:16:45 +0300

    initial commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const delayInput = document.querySelector('#delay');
+const startBtn = document.querySelector('.create');
+
+startBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const delayValue = Number(delayInput.value);
+  const selectedStateEl = document.querySelector('input[name="state"]:checked');
+
+  if (!selectedStateEl) {
+    alert('Please select a state!');
+    return;
+  }
+
+  const selectedState = selectedStateEl.value;
+
+  startBtn.disabled = true;
+
+  setTimeout(() => {
+    if (selectedState === 'fulfilled') {
+      iziToast.success({
+        title: 'Fulfilled',
+        message: `✅ Fulfilled promise in ${delayValue}ms`,
+        position: 'topRight',
+      });
+    } else {
+      iziToast.error({
+        title: 'Rejected',
+        message: `❌ Rejected promise in ${delayValue}ms`,
+        position: 'topRight',
+      });
+    }
+    startBtn.disabled = false;
+  }, delayValue);
+});
